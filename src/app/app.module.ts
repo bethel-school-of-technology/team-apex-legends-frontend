d35cb163d27f95241230096952ce3903a5851474
LandonRady <landonrady@yahoo.com>
2024-06-11 19:57:19 -0500

    Project set up components, services, etc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { CarListComponent } from './components/car-list/car-list.component';
+import { CarNewComponent } from './components/car-new/car-new.component';
+import { EditCarComponent } from './components/edit-car/edit-car.component';
+import { UserPageComponent } from './components/user-page/user-page.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
+import { AboutComponent } from './components/about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { NavigationPageComponent } from './navigation-page/navigation-page.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    SignInComponent,
+    CarListComponent,
+    CarNewComponent,
+    EditCarComponent,
+    UserPageComponent,
+    UserEditComponent,
+    AboutComponent,
+    DashboardComponent,
+    NavigationPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/car-new/car-new.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-new',
+  templateUrl: './car-new.component.html',
+  styleUrls: ['./car-new.component.css']
+})
+export class CarNewComponent {
+
+}
